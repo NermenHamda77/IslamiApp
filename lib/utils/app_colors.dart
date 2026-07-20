9b967f3c199a35fcb9671479520a80f14dd6c616
Nermen77 <nermeenzaitoon@gmail.com>
2026-07-20 23:07:30 +0300

    Completed radio tab ui.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -8,4 +8,6 @@ class AppColors{
   static const Color primaryColor = Color(0xffE2BE7F);
   static const Color brownColor = Color(0xff946107);
   static const Color grayColor = Color(0xff707070);
+  static const Color blackButtonColor = Color(0xB3202020);
+
 }
